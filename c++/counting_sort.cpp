008aa31bd05527ad100b6baf0a0c1b02906aeaf4
Nikolaj <Nikodyring@gmail.com>
2022-10-03 17:36:26 +0200

    vscode acting up

--- c++/counting_sort.cpp
+++ c++/counting_sort.cpp
@@ -1,38 +1,63 @@
+// C++ Program for counting sort
 #include <bits/stdc++.h>
+#include <string.h>
 using namespace std;
-#define ll long long
+#define RANGE 200
 
-//only for numbers in the range -1e3 to 1e3 : 
-void counting_sort(vector<ll> &v){
-    vector<ll> hash(int(1e6), 0) ;
-    for(auto x : v){
-        hash[int(1e3) + x]++;
-    }
-    v.clear();
-    for(int j = 0 ; j < hash.size() ; j++){
-        if(hash[j] > 0){
-            for(int i = 0 ; i < hash[j] ; i++){
-                v.push_back(j-int(1e3));
-            }
-        }
-    }
-}
-void solve(){
-    ll n ; cin >> n ;
-    vector<ll> v(n);
-    for(auto &x : v){
-        cin >> x;
-    }
-    counting_sort(v);
-    for(auto x : v){
-        cout << x << " ";
-    }
+// The main function that sort
+// the given string arr[] in
+// alphabetical order
+void countSort(char arr[])
+{
+	// The output character array
+	// that will have sorted arr
+	char output[strlen(arr)];
+
+	// Create a count array to store count of individual
+	// characters and initialize count array as 0
+	int count[RANGE + 1], i;
+	memset(count, 0, sizeof(count));
+
+	// Store count of each character
+	for (i = 0; arr[i]; ++i)
+		++count[arr[i]];
+
+	// Change count[i] so that count[i] now contains actual
+	// position of this character in output array
+	for (i = 1; i <= RANGE; ++i)
+		count[i] += count[i - 1];
+
+	// Build the output character array
+	for (i = 0; arr[i]; ++i) {
+		output[count[arr[i]] - 1] = arr[i];
+		--count[arr[i]];
+	}
+
+	/*
+	For Stable algorithm
+	for (i = sizeof(arr)-1; i>=0; --i)
+	{
+		output[count[arr[i]]-1] = arr[i];
+		--count[arr[i]];
+	}
+
+	For Logic : See implementation
+	*/
+
+	// Copy the output array to arr, so that arr now
+	// contains sorted characters
+	for (i = 0; arr[i]; ++i)
+		arr[i] = output[i];
 }
 
+// Driver code
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    solve();
+	char arr[] = "countingsortincpp";
+
+	countSort(arr);
+
+	cout << "Sorted character array is " << arr;
+	return 0;
 }
+
